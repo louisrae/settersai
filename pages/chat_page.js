53553d8c1227e0a2b5f5f836689b8db778e86583
louisrae <louisrae@settersandspecialists.com>
2022-11-26 13:42:58 -0600

    reformat chatui

--- pages/chat_page.js
+++ pages/chat_page.js
@@ -3,7 +3,6 @@ import React from "react";
 import ChatArea from "../components/ChatArea";
 import ChatList from "../components/ChatList";
 import MessageList from "../components/MessageList";
-
 import Sidebar from "../components/Sidebar";
 
 const our_messages = [
@@ -38,25 +37,30 @@ const Chat = () => {
   if (status === "authenticated") {
     return (
       <>
-        <Sidebar></Sidebar>
-        <div className="flex flex-1 flex-col md:pl-64">
-          <main className="flex-1">
-            <div class="flex-1 bg-gray-100 w-full h-full">
-              <div class="main-body container m-auto w-11/12 h-full flex flex-col">
-                <div class="main flex-1 flex flex-col">
-                  <div class="flex-1 flex h-full">
-                    <div class="sidebar hidden lg:flex w-1/3 flex-2 flex-col pr-6 ">
-                      <ChatList />
-                    </div>
-                    <div class="chat-area flex-1 flex flex-col">
-                      <MessageList></MessageList>
-                      <ChatArea></ChatArea>
+        <div class="w-full h-screen">
+          <div class="flex h-full">
+            <div class="hidden xl:block sm:flex-2 w-64 bg-gray-200">
+              <Sidebar></Sidebar>
+            </div>
+            3
+            <main className="flex-1">
+              <div class="flex-1 bg-gray-100 w-full h-full">
+                <div class="main-body container m-auto w-11/12 h-full flex flex-col">
+                  <div class="main flex-1 flex flex-col">
+                    <div class="flex-1 flex h-full">
+                      <div class="sidebar hidden lg:flex w-1/3 flex-2 flex-col pr-6 ">
+                        <ChatList />
+                      </div>
+                      <div class="chat-area flex-1 flex flex-col">
+                        <MessageList></MessageList>
+                        <ChatArea></ChatArea>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </main>
+            </main>
+          </div>
         </div>
       </>
     );
